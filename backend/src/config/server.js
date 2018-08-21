f659f19cf353afbc5f0f507e0259d982e23034d4
Fernando Soares <fernandosoares.dev@gmail.com>
2018-08-21 11:00:05 -0300

    Server setup

---
+++ backend/src/config/server.js
@@ -0,0 +1,10 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.listen(port, function() {
+    console.log(`Backend is running on port ${port}.`)
+})
