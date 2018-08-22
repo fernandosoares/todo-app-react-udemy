9162b7a187b54160f3dcce5ceeecdeb8c5087425
Fernando Soares <fernandosoares.dev@gmail.com>
2018-08-22 17:20:12 -0300

    Frontend + Navegacao

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        <h3>Nossa História</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h3>Missão</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h3>Visão</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h3>Valores</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
